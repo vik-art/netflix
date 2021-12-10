20682fa43f83bc6ee7df76a4c9ab4825ee1d085f
Viktoriia <krjuchenkova@gmail.com>
2021-12-10 14:13:27 +0200

    homepage markup finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
-import { ButtonComponent } from './components/button/button.component';
-import { HomepageContentComponent } from './components/homepage-content/homepage-content.component';
 import { HomepageComponent } from './modules/pages/homepage/homepage.component';
 import { LoginPageComponent } from './modules/pages/login-page/login-page.component';
 import { MenuListComponent } from './modules/shared/menu-list/menu-list.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/form.component';
 
 
 @NgModule({
@@ -18,10 +18,10 @@ import { MenuListComponent } from './modules/shared/menu-list/menu-list.componen
     HeaderComponent,
     HomepageComponent,
     LogoComponent,
-    ButtonComponent,
-    HomepageContentComponent,
     LoginPageComponent,
-    MenuListComponent
+    MenuListComponent,
+    FooterComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomepageContentComponent } from './homepage-content.component';
+import { FormComponent } from './form.component';
 
-describe('HomepageContentComponent', () => {
-  let component: HomepageContentComponent;
-  let fixture: ComponentFixture<HomepageContentComponent>;
+describe('FormComponent', () => {
+  let component: FormComponent;
+  let fixture: ComponentFixture<FormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomepageContentComponent ]
+      declarations: [ FormComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomepageContentComponent);
+    fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
