fd383941fdd87e2dd56b6ee633ed52a5728a81f7
Viktoriia <krjuchenkova@gmail.com>
2021-12-10 15:13:21 +0200

    logo component deleted, replaced markup to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoComponent } from './components/logo/logo.component';
 import { HomepageComponent } from './modules/pages/homepage/homepage.component';
 import { LoginPageComponent } from './modules/pages/login-page/login-page.component';
 import { MenuListComponent } from './modules/shared/menu-list/menu-list.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { FormComponent } from './components/form/form.component';
 
 
 @NgModule({
@@ -17,11 +15,9 @@ import { FormComponent } from './components/form/form.component';
     AppComponent,
     HeaderComponent,
     HomepageComponent,
-    LogoComponent,
     LoginPageComponent,
     MenuListComponent,
     FooterComponent,
-    FormComponent,
   ],
   imports: [
     BrowserModule,
